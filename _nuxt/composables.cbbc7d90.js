14874c5f79bf34521da4a5bbfd85988a593ca015
persteenolsen <38952136+persteenolsen@users.noreply.github.com>
2023-11-30 17:47:56 +0000

    Deploying to gh-pages from @ persteenolsen/nuxt-3-ionic-capacitor-part-2-gh-pages@2e5f4a2462cb1aaf8183ce438bd24303811a25b0 🚀

---
+++ _nuxt/composables.cbbc7d90.js
@@ -0,0 +1,1 @@
+import{J as r}from"./entry.816c3e57.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ _nuxt/ionicapp.3a9ba554.js
@@ -0,0 +1,1 @@
+import{u as a}from"./composables.cbbc7d90.js";import{c as t,w as o,u as r,o as s,G as i,H as c}from"./entry.816c3e57.js";const m={__name:"ionicapp",setup(p){return a({viewport:"width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"}),(e,l)=>(s(),t(r(c),null,{default:o(()=>[i(e.$slots,"default")]),_:3}))}};export{m as default};
